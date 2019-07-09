68b3506101b465b6b24cacd85918e899de9173ca
Scott Serok <scott@renofi.com>
2019-07-09 14:29:15 -0400

    catch signals and terminate gracefully

--- lib/salesforce_streamer/server.rb
+++ lib/salesforce_streamer/server.rb
@@ -13,6 +13,22 @@ module SalesforceStreamer
     def run
       @client.authenticate!
       @logger.info 'Starting Server'
+      catch_signals
+      start_em
+    end
+
+    private
+
+    def catch_signals
+      %w[INT USR1 USR2 TERM TTIN TSTP].each do |sig|
+        trap sig do
+          puts "Caught signal #{sig}. Shutting down..."
+          exit 0
+        end
+      end
+    end
+
+    def start_em
       EM.run do
         @push_topics.each do |topic|
           @client.subscribe(topic.name, replay: topic.replay) do |msg|
